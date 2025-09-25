67509fcad3a6a036f610bfba71ea189d387d50e6
kaustav <75007246+KaustavDey357@users.noreply.github.com>
2025-09-25 15:03:31 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,38 @@
+terraform {
+   required_providers{
+       docker ={
+         source='kreuzwerker/docker'
+         version ='~> 3.0'
+       }
+    }}
+   provider "docker" {}
+   
+   resource "docker_image" "node_app"{
+     name ="node:18"
+   }
+   resource "docker_container" "node_container" {
+      name ="node_container"
+      image = docker_image.node_app.latest
+      
+      command = ["tail", "-f","/dev/null"]#to keep the container running 
 
+      mounts {
+        target ="/usr/src/app/app.js"
+        source ="./app.js"
+        type ="bind"
+      }
+
+      mounts {
+        target ="/usr/src/app/public"
+        source ="./public"
+        type ="bind"
+      }
+      working_dir  = "/usr/src/app"
+
+      ports {
+      internal = 4000
+      external = 4000
+      }
+   }
+
+   
